03b544e19ea5fcea69a1a7497c56da7a5d4b0cd8
Srishanth5 <srishanthreddytumma1@gmail.com>
2026-03-02 23:52:56 +0530

    Assignment3

--- IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST03/task.py
+++ IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST03/task.py
@@ -1,19 +1,11 @@
-def Student_Grade_System(name: str, n1: int, n2: int, n3: int) -> str:
-    # Calculate the average
-    average = (n1 + n2 + n3) / 3
+def Student_Grade_System(name:str,n1: int,n2: int,n3: int) -> str:
+    grades = [n1, n2, n3]
+    avg = int(sum(grades) * 100 / 3) / 100
+    status = "Pass" if all(grade >= 35 for grade in grades) else "fail"
+    return f"Average grade: {avg}, Status: {status}"
     
-   
-    status = "Pass" if average >= 50 else "Fail"
-    
-    
-    return f"Average grade: {average:.2f}, Status: {status}"
 
 if __name__ == '__main__':
-    
-    name = input().strip()
-    
-    try:
-        n1, n2, n3 = list(map(int, input().split()))
-        print(Student_Grade_System(name, n1, n2, n3))
-    except ValueError:
-        print("Please enter three numerical grades separated by spaces.")
+    name = input()
+    n1,n2,n3 = list(map(int,input().split()))
+    print(Student_Grade_System(name,n1,n2,n3))
