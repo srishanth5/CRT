518ff3cee95eb345f275241de6e64f45b74fe553
Srishanth5 <srishanthreddytumma1@gmail.com>
2026-03-02 15:57:11 +0530

    Assignmnents Submitted

--- IV-SEM/Python/Assignments/ M01_Language_&_Programming_Basics/S04/PS06_loop_control.py
+++
@@ -1,9 +0,0 @@
-''' password retry system(max 3 attempts) ,if password is correct show login successful '''
-p1 = "abc123"
-for i in range(3):
-    p2 = input("Enter your password: ")
-    if p1 == p2:
-        print("Login successful")
-        break
-else:
-    print("Account locked") 
