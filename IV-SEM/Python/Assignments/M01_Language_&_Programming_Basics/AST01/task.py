aebfe7ff9f00dd423094734e5e7f3d1f27855fe6
Srishanth5 <srishanthreddytumma1@gmail.com>
2026-03-02 13:58:17 +0530

    fixed structure for Saarthaak

--- IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST01/task.py
+++ IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST01/task.py
@@ -1,11 +1,11 @@
 def Ticket_Pricing(n: int) -> int:
        if n < 5:
-            return 0
+             return 0
        elif 5 <= n <= 17:
              return 10
        elif 18 <= n <= 64:
              return 20
-       else:  # n >= 65
+       else:
              return 15
        
    
